a5538cca3203cf821f8382c8a305c355c3f89071
bangtoy <muha.toyyib@gmail.com>
2021-03-16 04:44:06 +0700

    First animation with GSAP

--- src/App.js
+++ src/App.js
@@ -1,51 +1,49 @@
-import React, { useRef, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-
 import { TweenMax, Power3 } from 'gsap';
 
 function App() {
-  let logoItem = useRef(null);
-  let textItem = useRef(null);
-  useEffect(() => {
-    console.log(logoItem);
-    TweenMax.to(
-      logoItem, .8,
-      {
-        opacity: 1,
-        y: -20,
-        ease: Power3.easeOut
-      }
-    )
-    console.log(textItem);
-    TweenMax.to(
-      textItem, .8,
-      {
-        opacity: 1,
-        y: -20,
-        ease: Power3.easeOut,
-        delay: 0.2
-      }
-    )
-  })
+  let app = useRef(null)
+  let circle = useRef(null)
+  let circleRed = useRef(null)
+  let circleBlue = useRef(null)
 
+  const [state, setState] = useState(false)
+  const handleExpand = () => {
+    TweenMax.to(circleRed, .8, { width: 200, height: 200, ease: Power3.easeOut })
+    setState(true)
+  }
+  const handleShrink = () => {
+    TweenMax.to(circleRed, .8, { width: 75, height: 75, ease: Power3.easeOut })
+    setState(false)
+  }
+
+
+  useEffect(() => {
+    TweenMax.to(app, 0, { css: { visibility: 'visible' } })
+    // TweenMax.from(circle, .8, { opacity: 0, x: 40, ease: Power3.easeOut })
+    // TweenMax.from(circleRed, .8, { opacity: 0, x: 40, ease: Power3.easeOut, delay: .2 })
+    // TweenMax.from(circleBlue, .8, { opacity: 0, x: 40, ease: Power3.easeOut, delay: .4 })
+    //or siplify
+    TweenMax.staggerFrom([circle, circleRed, circleBlue], .8, { opacity: 0, x: 40, ease: Power3.easeOut }, .2)
+  }, [])
   return (
-    <div className="App">
+    <div
+      ref={el => app = el}
+      className="App">
       <header className="App-header">
-        <img
-          ref={el => { logoItem = el }}
-          src={logo} className="App-logo" alt="logo" />
-        <p ref={el => { textItem = el }}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="circle-container">
+          <div
+            ref={el => circle = el}
+            className="circle"></div>
+          <div
+            onClick={state !== true ? handleExpand : handleShrink}
+            ref={el => circleRed = el}
+            className="circle red"></div>
+          <div
+            ref={el => circleBlue = el}
+            className="circle blue"></div>
+        </div>
       </header>
     </div>
   );
